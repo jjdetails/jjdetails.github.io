28b927b90c78bffbabfea0234b0b95d79b3fd9a6
EndOfFile <git.unf.404@gmail.com>
2022-09-30 17:35:10 +0200

    Item List

---
+++ items/script.js
@@ -0,0 +1,19 @@
+const main = document.getElementById('main')
+
+let content = "<div class=\"info-grid\">"
+
+content += "<div class=\"info-table\">"
+content += "<div class=\"category-title\">Items</div>"
+
+for (let item of ITEMS) {
+	content += `<span>
+		<span class='item-img'><img src="items/${item.id}.webp" loading="lazy"></span>
+		<span class='item-details'><span>${item.n}</span><span></span></span>
+	</span>`
+}
+
+content += "</div>" // end category
+
+content += "</div>" // end of info grid
+
+main.innerHTML = content	
